7a787f1bc378865c2b5e6bb501d6bcb7a24e73ff
Richwell111 <bcict2013@gmail.com>
2024-12-09 15:00:50 +0000

    first commit

---
+++ src/data.js
@@ -0,0 +1,22 @@
+const projects = [
+  {
+    title: "birthday buddy",
+    url: "https://birthdayreact1.netlify.app/",
+    image: "./assets/birthday.png",
+  },
+  {
+    title: "tours",
+    url: "https://toursreactfundamentalsproject.netlify.app/",
+    image: "./assets/tours.png",
+  },
+  {
+    title: "reviews",
+    url: "https://reviews-react-fundamentals-project.netlify.app/",
+    image: "./assets/reviews.png",
+  },
+  {
+    title: "questions",
+    url: "https://react-fundamentals-questions-project.netlify.app/",
+    image: "./assets/questions.png",
+  },
+];
