78ad2bcd75ed06d47c519be580bcec10697c2409
Richwell111 <bcict2013@gmail.com>
2025-01-06 17:49:47 +0000

    made changes to the hero header and paragraph

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -4,13 +4,13 @@ export const Hero = () => {
     <section className="hero">
       <div className="hero-center">
         <div className="hero-title">
-          <h1>React Projects</h1>
+          <h1>React and Next.js Projects</h1>
           <p>
-            Single-origin coffee deep v sus small batch. Gorpcore vape
-            lumbersexual normcore four dollar toast drinking vinegar. Twee 90's
-            taiyaki small batch bitters, bespoke jianbing leggings marxism
-            keytar pabst adaptogen mixtape tumblr stumptown. DSA twee iceland
-            tacos swag tote bag. Solarpunk man bun fingerstache retro pinterest.
+            Welcome to my portfolio! Here, I showcase my journey in web
+            development through a curated collection of React and Next.js
+            projects. Each project demonstrates my expertise in building modern,
+            responsive, and dynamic web applications. Explore my work to see how
+            I bring ideas to life with code!
           </p>
         </div>
         <div className="img-container">
